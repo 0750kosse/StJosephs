43a5950f4e9ca7b90eeb0545e4fa4c4b345c8c85
Jose Sanchez <sanchezmartinezjosecarlos@gmail.com>
2021-01-05 18:26:00 +0000

    Slider functionality working

--- public_html/wpscripts/index.js
+++ public_html/wpscripts/index.js
@@ -1,82 +1,60 @@
 const mobileMenu = document.querySelector(".header-hamburguer");
 const menuLinks = document.querySelector(".nav-links");
-const slideshow = document.querySelector('.slide-wrap');
 
 function mobileLinksToggle() {
   if (menuLinks.style.display === "flex") {
-    mobileMenu.className = "header-hamburguer fa fa-bars fa-4x"
+    mobileMenu.className = "header-hamburguer fa fa-bars fa-2x"
     menuLinks.style.display = "none";
   } else {
     menuLinks.style.display = "flex";
-    menuLinks.className = "active";
-    mobileMenu.className = "header-hamburguer fa fa-times fa-4x"
+    menuLinks.className = "operative";
+    mobileMenu.className = "header-hamburguer fa fa-times fa-2x"
   }
 }
 
-if (slideshow != null) {
+let currentSlide = 0;
+const slides = document.querySelectorAll(".slide")
+const dots = document.querySelectorAll('.dot')
 
-  let slides = document.querySelectorAll('.slide-entry'),
-    slideCount = slides.length,
-    currentSlide = 0,
-    slideHeight = null,
-    initialHeight = slides[0].clientHeight;
-
-
-  slides[0].classList.add('active'); //on load, activate the 1st slide
-
-  function moveToSlide(n) { // set up slide navigation functionality
-    slides[currentSlide].className = 'slide-entry';
-    currentSlide = (n + slideCount) % slideCount;
-    slides[currentSlide].className = 'slide-entry active';
-    slideHeight = slides[currentSlide].clientHeight;
-    slideshow.style.height = slideHeight + 'px';
-    window.addEventListener('resize', function () {
-      resizedSlideHeight = slides[currentSlide].clientHeight;
-      slideshow.style.height = resizedSlideHeight + 'px';
-    });
-  }
-
-  function nextSlide(e) {
-    moveToSlide(currentSlide + 1);
-    let code = e.keyCode;
-    if (code == 39) {
-      moveToSlide(currentSlide + 1);
-    }
-  };
-  function prevSlide(e) {
-    moveToSlide(currentSlide + -1);
-    let code = e.keyCode;
-    if (code == 37) {
-      moveToSlide(currentSlide + -1);
-    }
-  };
-
-  const slideHandlers = {
-    nextSlide: function (element) {
-      document.querySelector(element).addEventListener('click', nextSlide);
-      document.body.addEventListener('keydown', nextSlide, false);
-    },
-    prevSlide: function (element) {
-      document.querySelector(element).addEventListener('click', prevSlide);
-      document.body.addEventListener('keydown', prevSlide, false);
-    }
-  }
-
-  slideHandlers.nextSlide('#next-slide');
-  slideHandlers.prevSlide('#prev-slide');
-
-  setInterval(function () { // Automatic slideshow
-    nextSlide();
-  }, 8000);
-
-  // Dynamic slideshow height
-  slideshow.style.height = initialHeight + 'px'; //on load, set the height of the slider to the first active slide
-
-  window.addEventListener('resize', function () { // adjust the height of the slidehow as the browser is resized
-    let resizedHeight = slides[0].clientHeight;
-    slideshow.style.height = resizedHeight + 'px';
-  });
+const init = (n) => {
+  slides.forEach((slide, index) => {
+    slide.style.display = "none"
+    dots.forEach((dot, index) => {
+      dot.classList.remove("active")
+    })
+  })
+  slides[n].style.display = "block"
+  dots[n].classList.add("active")
 }
+
+document.addEventListener("DOMContentLoaded", init(currentSlide))
+
+const next = () => {
+  currentSlide >= slides.length - 1 ? currentSlide = 0 : currentSlide++
+  init(currentSlide)
+}
+
+const prev = () => {
+  currentSlide <= 0 ? currentSlide = slides.length - 1 : currentSlide--
+  init(currentSlide)
+}
+
+document.querySelector(".next").addEventListener('click', next)
+document.querySelector(".prev").addEventListener('click', prev)
+
+
+setInterval(() => {
+  next()
+}, 7000);
+
+dots.forEach((dot, i) => {
+  dot.addEventListener("click", () => {
+    init(i)
+    currentSlide = i
+  })
+})
+
+
 
 
 
